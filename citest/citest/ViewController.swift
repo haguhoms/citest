da8455e624819f708ba953253b0e99da0542a337
masaki hasegawa <hasegawa@masakinoMacBook-Pro.local>
2017-08-09 17:45:27 +0900

    create project

---
+++ citest/citest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  citest
+//
+//  Created by masaki hasegawa on 2017/08/09.
+//  Copyright © 2017年 Masaki Hasegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
